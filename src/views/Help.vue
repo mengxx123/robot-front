d1ef8d33a714d27408d0932707856f4cccf10955
cjh <1418503647@qq.com>
2019-01-20 03:01:10 +0800

    mac

---
+++ src/views/Help.vue
@@ -0,0 +1,41 @@
+<template>
+    <my-page title="帮助">
+        <ul class="cmd-list">
+            <li class="item">
+                百度 在线工具
+
+                工具 gif
+
+                大写 asd
+                ASD
+
+                小写 ASD
+                asd
+
+                
+
+                图片
+                回复
+            </li>
+        </ul>
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+            }
+        },
+        mounted() {
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.cmd-list {
+    .item {
+        border: 1px solid #eee;
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <my-page title="机器人">
+        开发中
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                page: {
+                    menu: [
+                        {
+                            type: 'icon',
+                            icon: 'help',
+                            to: '/help'
+                        }
+                    ]
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
